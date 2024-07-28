6ea8a02e8c27c7f05ece4dd3899f304206f23ea6
rajZadafiya55 <zadagfiyaraj395.com>
2024-07-28 22:50:37 +0530

    views config

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const QRCode = require("qrcode");
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 function isValidUrl(url) {
   const regex = /^(http:\/\/|https:\/\/|www\.)/i;
