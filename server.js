ed3f5d89e49d21d162f1e537631bf29f39e6d62d
rajZadafiya55 <zadagfiyaraj395.com>
2024-07-28 22:42:03 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const QRCode = require("qrcode");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+function isValidUrl(url) {
+  const regex = /^(http:\/\/|https:\/\/|www\.)/i;
+  return regex.test(url);
+}
+
+app.get("/", (req, res) => {
+  res.render("index", { error: null });
+});
+
+app.post("/generate", (req, res) => {
+  const url = req.body.url;
+
+  if (!isValidUrl(url)) {
+    return res.render("index", {
+      error: "Please enter a valid URL.",
+    });
+  }
+
+  QRCode.toDataURL(url, (err, src) => {
+    if (err) res.send("Error occured.!");
+    res.render("generate", { src });
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server started on http://localhost:3000");
+});
